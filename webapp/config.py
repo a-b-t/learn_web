ce9800c29642bb53685c55111072597190ae19d0
Alexandr Timonin <a-b-t@yandex.ru>
2019-06-10 21:30:57 +0300

    поддержка БД Flask-SQLAlchemy

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 WEATHER_DEFAULT_CITY = 'Moscow,Russia'
 WEATHER_API_KEY = '04a4a202c86048e19bb74339192905'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
